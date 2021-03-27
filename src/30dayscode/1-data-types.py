13dbc0fc6c45bac366b1a6fefee75f9a24cf5656
Johannes Drechsler <JohannesDrechslerUS@gmail.com>
2021-03-27 14:27:27 +0000

    added more practices

---
+++ src/30dayscode/1-data-types.py
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+print("enter any positive number")
+user_num = int(input())
+
+print("enter float / decimal number")
+user_float = float(input())
+
+print("enter text")
+user_text = input()
+
+print(i+user_num)
+print(d+user_float)
+print(s+user_text)
---
+++ src/practice/runnerUp.py
@@ -0,0 +1,7 @@
+n = 5
+arr = [2,3,6,6,5]
+
+hashSet = set(arr)
+sortedArr = sorted(hashSet, reverse=True)
+
+print(sortedArr[1])
